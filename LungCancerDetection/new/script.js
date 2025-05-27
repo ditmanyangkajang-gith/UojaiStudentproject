893c0c96e572a401c849cb1f6cb111190cc19edb
Dit Manyang Kajang <ditahon123@gmail.com>
2025-05-27 21:51:00 +0200

    Add files via upload

---
+++ LungCancerDetection/new/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any simple JavaScript functionality here if needed.
+    console.log("UGG Website Loaded with more sections!");
+});
---
+++ LungCancerDetection/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any simple JavaScript functionality here if needed.
+    // For a basic project page, you might not need much.
+    console.log("GitHub Page Loaded!");
+});
